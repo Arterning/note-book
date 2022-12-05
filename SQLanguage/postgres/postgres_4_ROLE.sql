9ba836cb6185638b7d569e7628f99c6b74bd04e2
Arterning <ning.huang.ridson@gmail.com>
2022-12-05 16:41:51 +0800

    extract sql
    Former-commit-id: cb5243cff8b3b942829619f4069d8bb715c4b390 [formerly 02dd66512e5ccd9a6a9e7f9ffab48df97c541c3f] [formerly a6625eefdc5ee9237779f2af54817c8bb7ea6d83 [formerly b5aba10cc74a140c48abb8a9f9e97cf43e6262bc]] [formerly b5b0450bf640f15efebb6acf1bb43c40680a0ccc [formerly 11f0c30ba0b14975b054f779bd6569b0e33381ec] [formerly 91188b0ca0f57a4cbbe2e5d3723c8c2de705b847 [formerly 6e6becf36116663b13324c0f60b6c988f744b1a3] [formerly aae9160093e3d11de7611e9bc9682214d38e530e]]]
    Former-commit-id: 844509302681e4d062d7a4acdbef831c81c931c0
    Former-commit-id: 21f56680dc85086649adc596fcc0edee5733c639 [formerly 9b8d4d5b428de1823e93e1aaeaf4e8579fc5b626]
    Former-commit-id: b9e0be7765a3282b7687ffaf111015b7a5dc39c2
    Former-commit-id: fd11f5d59e726a5f3882326b5ec104e20a3e23d3

--- SQLanguage/postgres/postgres_4_ROLE.sql
+++
@@ -1,8 +0,0 @@
-CREATE USER ningHuang WITH PASSWORD 'ningHuang';
-GRANT ALL ON COMPANY TO ningHuang;
-REVOKE ALL ON COMPANY FROM ningHuang;
-drop user ningHuang;
-
--- GRANT privilege [, ...]
--- ON object [, ...]
--- TO { PUBLIC | GROUP group | username }
