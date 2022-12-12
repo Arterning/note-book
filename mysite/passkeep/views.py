48fa4637f394c8da64a9abe6e3768325c89c9c42
Arterning <ning.huang.ridson@gmail.com>
2022-12-12 15:30:34 +0800

    add form

--- mysite/passkeep/views.py
+++ mysite/passkeep/views.py
@@ -4,15 +4,30 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from .models import TokenInfo
 from django.template import loader
+from .forms import TokenForm
+from django.http import HttpResponseRedirect
 
 def index(request):
     return HttpResponse("Hello, world. You're at the passkeep index.")
 
 def addToken(request):
-    context = {
-    }
-    template = loader.get_template('passkeep/add.html')
-    return HttpResponse(template.render(context, request))
+    # if this is a POST request we need to process the form data
+    if request.method == 'POST':
+        # create a form instance and populate it with data from the request:
+        form = TokenForm(request.POST)
+        # check whether it's valid:
+        if form.is_valid():
+            # process the data in form.cleaned_data as required
+            # ...
+            # redirect to a new URL:
+            # print(form)
+            return HttpResponseRedirect('/thanks/')
+
+    # if a GET (or any other method) we'll create a blank form
+    else:
+        form = TokenForm()
+
+    return render(request, 'passkeep/add.html', {'form': form})
 
 def allToken(request):
     resp = TokenInfo.objects.all()
@@ -21,6 +36,22 @@ def allToken(request):
         'token_list': resp,
     }
     return HttpResponse(template.render(context, request))
+    # if this is a POST request we need to process the form data
+    if request.method == 'POST':
+        # create a form instance and populate it with data from the request:
+        form = TokenForm(request.POST)
+        # check whether it's valid:
+        if form.is_valid():
+            # process the data in form.cleaned_data as required
+            # ...
+            # redirect to a new URL:
+            return HttpResponseRedirect('/thanks/')
+
+    # if a GET (or any other method) we'll create a blank form
+    else:
+        form = TokenForm()
+
+    return render(request, 'name.html', {'form': form})
 
 
 def detail(request, token_id):
