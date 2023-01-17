8e2f9657412d8ad61f96de2b62204fae66ae3f56
Arterning <ning.huang.ridson@gmail.com>
2023-01-17 15:00:40 +0800

    commit js
    Former-commit-id: 74068bdce6cda357224178d2d0620acd9db40641

---
+++ UI/lib package/vite/base.js
@@ -0,0 +1,22 @@
+/**
+ * # get started
+
+使用 vite 初始化项目感觉比较简单 好像有几条命令都可以 然后选择 react/vuex name typescript/javascript 就可以了
+
+```shell
+使用 NPM:
+
+$ npm create vite@latest
+使用 Yarn:
+
+$ yarn create vite
+
+
+好像还可以
+
+npm init vite
+
+yarn init vite
+```
+
+ */
