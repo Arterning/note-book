1d674c63c28e5652ef9ac47508c7bf5a4a96720f
Arterning <ning.huang.ridson@gmail.com>
2023-01-17 16:29:29 +0800

    commit js ess
    Former-commit-id: 4d2036d9ee9bb4ce21e114400883913c7b54c233

---
+++ UI/lib package/api/基于fetch的调用封装.js
@@ -0,0 +1,25 @@
+export class ApiError extends Error {
+    constructor(url, status) {
+        super(`'${url}' returned ${status}`);
+        if (Error.captureStackTrace) {
+            Error.captureStackTrace(this, ApiError);
+        }
+        this.name = 'ApiError';
+        this.status = status;
+    }
+}
+
+export async function fetchJson(url, options) {
+    const response = await fetch(url, options);
+    if (!response.ok) {
+        throw new ApiError(url, response.status);
+    }
+    return await response.json();
+}
+
+
+await fetchJson("url", {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify({ identifier: email, password }),
+});
