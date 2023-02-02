abeb033808d1fe08132ec8c44064ee855f193372
Arterning <ning.huang.ridson@gmail.com>
2023-02-02 11:56:45 +0800

    c
    Former-commit-id: 66adfaf3d51e7464525af25a591b2ae0431d6f46

---
+++ service/git/03-git修改提交时间.js
@@ -0,0 +1,16 @@
+/**
+
+Thanks to ChatGPT
+
+Set the environment variables:
+
+$ export GIT_AUTHOR_DATE="<desired date>"
+$ export GIT_COMMITTER_DATE="<desired date>"
+Replace "<desired date>" with the desired date and time, in the format "YYYY-MM-DD HH:MM:SS".
+
+export GIT_COMMITTER_DATE="2023-01-26 08:50:55"
+export GIT_AUTHOR_DATE="2023-01-19 08:50:55"
+
+Create the commit:
+$ git commit -m "<commit message>"
+ */
