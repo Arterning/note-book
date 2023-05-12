b2f68ef60dae2cb577465ba2d4c5b77fd7926066
babyNing <ning.huang.ridson@gmail.com>
2023-05-12 07:43:20 +0000

    Extract note to notion clean the repository

--- code-note/UI/lib package/包管理工具/npm/设置代理.js
+++
@@ -1,4 +0,0 @@
-// 假设本地代理端口为8080
-// npm config set proxy "http://localhost:8080"
-// npm config set https-proxy "http://localhost:8080"
-
--- code-note/UI/lib package/包管理工具/pnpm/base.js
+++
@@ -1,7 +0,0 @@
-/**
- * 最好的解决方案
- */
-/**
- install
- npm install -g pnpm
- */
