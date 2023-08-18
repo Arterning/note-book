2c9e7dbf6708a114873ec266269ea09cf7db61b1
Arterning <ning.huang.ridson@gmail.com>
2023-08-18 13:44:18 +0800

    add python script

--- computer-science/rerange.py
+++ computer-science/rerange.py
@@ -3,21 +3,20 @@ import shutil
 
 
 def process_files(directory):
-    for root, _, files in os.walk(directory):
-        for filename in files:
-            if "-" in filename:
-                parts = filename.split("-")
-                if len(parts) > 1:
-                    prefix = parts[0]
-                    new_directory = os.path.join(root, prefix)
-                    if not os.path.exists(new_directory):
-                        os.makedirs(new_directory)
+    for filename in os.listdir(directory):
+        if "-" in filename:
+            parts = filename.split("-")
+            if len(parts) > 1:
+                prefix = parts[0]
+                new_directory = os.path.join(directory, prefix)
+                if not os.path.exists(new_directory):
+                    os.makedirs(new_directory)
 
-                    source_path = os.path.join(root, filename)
-                    destination_path = os.path.join(new_directory, filename)
+                source_path = os.path.join(directory, filename)
+                destination_path = os.path.join(new_directory, filename)
 
-                    shutil.move(source_path, destination_path)
-                    print(f"Moved: {source_path} -> {destination_path}")
+                shutil.move(source_path, destination_path)
+                print(f"Moved: {source_path} -> {destination_path}")
 
 
 if __name__ == "__main__":
